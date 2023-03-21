0338047a49fc0c781d422e472fb16c23e4cea1f3
JackArkwright <17arkwright_j@bexleygs.co.uk>
2023-03-21 09:25:30 +0000

    login system partly done

--- src/FileHandling.java
+++ src/FileHandling.java
@@ -1,6 +1,8 @@
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.BufferedReader;
+import java.nio.Buffer;
+import java.nio.BufferOverflowException;
 import java.util.ArrayList;
 import java.io.PrintWriter;
 import java.io.FileWriter;
@@ -11,7 +13,7 @@ public class FileHandling {
 
     //Allows for one line of the file to be accessed and returned to other methods for UI or the user
     public static String singleLine (String filename){
-        try (BufferedReader br = new BufferedReader(new FileReader(String filename))){
+        try (BufferedReader br = new BufferedReader(new FileReader(filename))){
             line = br.readLine();
         } catch (IOException e) {
             System.out.println("Data not found");
@@ -22,13 +24,30 @@ public class FileHandling {
 
     public static ArrayList<String> wholeFileRead (String filename){
         ArrayList<String> fileOutput = new ArrayList<String>();
-        try (BufferedReader br = new BufferedReader (new FileReader (String filename))){
+        try (BufferedReader br = new BufferedReader (new FileReader (filename))){
             line = br.readLine();
 
             while (line != null){
                 fileOutput.add(line);
                 line = br.readLine();
             }
+        } catch (IOException e) {
+            System.out.println("There was an error reading the file");
+        } return fileOutput;
+    }
+
+    public static void fileWriter (String filename) {
+        try (PrintWriter pw = new PrintWriter(new FileWriter(filename))){
+
+        } catch (IOException e){
+            System.out.println("There was an unexpected error");
         }
+    }
+    // Searches through a file with a list of usernames and passwords
+    // Returns true or false based on whether the login has been found
+    public static boolean login (String username, String password) {
+        line = singleLine("logins.txt");
+        String[] splitString = line.split(", ");
+
     }
 }
--- src/Member.java
+++ src/Member.java
@@ -3,10 +3,22 @@ public class Member {
     private String name;
     private char level; // red 'R', orange 'O', green 'G', yellow 'Y', adult 'A', coach 'C'
 
-    public Member (String name, int age, char level){
+    public Member (String name, int age, char level) {
         this.name = name;
         this.age = age;
         this.level = level;
+    }
+    //Get the members name
+    public String getName(){
+        return name;
+    }
+    //Get the members age
+    public int getAge(){
+        return age;
+    }
+    //Get the members level
+    public char getLevel(){
+        return level;
     }
 
 }
